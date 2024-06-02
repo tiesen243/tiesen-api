ebfda1f5b1fb86ea04848775d211fabf80832793
Tiesen <ttien56906@gmail.com>
2024-06-03 05:22:56 +0700

    feat: cors

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
   images: { unoptimized: true },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  async headers() {
+    return [{ headers: [{ key: 'Access-Control-Allow-Origin', value: '*' }] }]
+  },
 }
 
 export default nextConfig
