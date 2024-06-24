459865584327f8bc7790cf56d51809f7764fd2e9
tiesen243 <ttien56906@gmail.com>
2024-06-25 06:43:25 +0700

    chore: trigger deploy

--- app/api/send-mail/route.ts
+++ app/api/send-mail/route.ts
@@ -16,6 +16,7 @@ const schema = z.object({
 export const POST = async (req: NextRequest) => {
   try {
     const parsed = schema.parse(await req.json())
+
     if (parsed.api_key !== process.env.API_KEY)
       return NextResponse.json({ error: 'Invalid API key' }, { status: 401 })
 
